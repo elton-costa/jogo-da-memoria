5a3f19e77b7bf564d29aa506d7f03ff4266a0803
erdac <tonrc87@gmail.com>
2020-04-05 15:28:32 -0300

    022-02 Feito o componente do memory-card -front em JS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
-const $icon = `
+const $memoryCardFront = document.createElement("article");
+
+const $iconCollab = `
 <img 
     src='img/icon-collabcode.png' 
     alt='Gueio mascote da CollabCode' 
     class='icon'
 >`;
 
-$memoryCard.classList.add("memory-card");
+const $iconC = `
+<img 
+    src='img/icon-c.png' 
+    alt=Ícone de um livro da linguagem C++'' 
+    class='icon'
+>`;
 
+$memoryCard.classList.add("memory-card");
 $root.insertBefore($memoryCard, null);
-$memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, $memoryCard);
+
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
