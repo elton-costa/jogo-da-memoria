3af1ee162a2df4886eb828a98fde3d1c2706d88f
erdac <tonrc87@gmail.com>
2020-04-07 18:20:44 -0300

    023-02 Pages organizadas

---
+++ src/pages/game/index.js
@@ -0,0 +1,18 @@
+const $root = document.querySelector("#root");
+const $memoryCardFront = document.createElement("article");
+
+createMemoryCard();
+
+const $iconC = `
+<img 
+    src='img/icon-c.png' 
+    alt=Ícone de um livro da linguagem C++'' 
+    class='icon'
+/>`;
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, null);
+
+   
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
