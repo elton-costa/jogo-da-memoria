30c0b88d2d80604e5a4da7a75c32bbdb1b22db49
erdac <tonrc87@gmail.com>
2020-04-07 18:29:10 -0300

    023-03 Código isolado do memory card front

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,18 +1,11 @@
-const $root = document.querySelector("#root");
-const $memoryCardFront = document.createElement("article");
-
 createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCardFront();
+createMemoryCardFront();
 
-const $iconC = `
-<img 
-    src='img/icon-c.png' 
-    alt=Ícone de um livro da linguagem C++'' 
-    class='icon'
-/>`;
-
-$memoryCardFront.classList.add("memory-card");
-$memoryCardFront.classList.add("-front");
-$root.insertBefore($memoryCardFront, null);
-
-   
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
