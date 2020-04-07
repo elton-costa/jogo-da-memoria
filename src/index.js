06a2869fe55719a67e28d0d0052b759a41b8d176
erdac <tonrc87@gmail.com>
2020-04-07 18:04:49 -0300

    023-01 Entendendo o uso das funções

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 const $root = document.querySelector("#root");
-const $memoryCard = document.createElement("article");
 const $memoryCardFront = document.createElement("article");
-
-const $iconCollab = `
-<img 
-    src='img/icon-collabcode.png' 
-    alt='Gueio mascote da CollabCode' 
-    class='icon'
->`;
+createMemoryCard();
 
 const $iconC = `
-<img 
-    src='img/icon-c.png' 
-    alt=Ícone de um livro da linguagem C++'' 
-    class='icon'
->`;
+    <img
+        src='img/icon-c.png'
+        alt='Ícone do C++'
+        class='icon'
+    />
+    `;
 
-$memoryCard.classList.add("memory-card");
-$root.insertBefore($memoryCard, null);
+    $memoryCardFront.classList.add("memory-card");
+    $memoryCardFront.classList.add("-front");
+    $root.insertBefore("afterbegin", null);
 
-$memoryCardFront.classList.add("memory-card");
-$memoryCardFront.classList.add("-front");
-$root.insertBefore($memoryCardFront, $memoryCard);
-
-$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+    $memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
