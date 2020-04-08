0ef0d88418281148ed11195c0a0e8b4dac0a264c
erdac <tonrc87@gmail.com>
2020-04-08 18:24:51 -0300

    024-02 Usando o atributo onclick

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -5,6 +5,7 @@ function createMemoryCard() {
                 src='img/icon-collabcode.png'
                 alt='Gueio mascote da CollabCode'
                 class='icon'
+                onClick='handleClick()'
             />
         </article>
     `;
@@ -16,12 +17,17 @@ function createMemoryCardFront() {
     const $memoryCard = `
         <article class="memory-card -front">
             <img
-                src='img/icon-collabcode.png'
+                src='img/icon-c.png'
                 alt='Gueio mascote da CollabCode'
                 class='icon'
+                onClick='handleClick()'
             />
         </article>
     `;
 
     return $memoryCard;
+}
+
+function handleClick() {
+    console.log("ae");
 }
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,7 +1,18 @@
-const cardsWrapper = createCardsWrapper();
-const memoryCard = createMemoryCard();
-const memoryCardFront = createMemoryCardFront();
+const $root = document.querySelector("#root");
 
-console.log(cardsWrapper);
-console.log(memoryCard);
-console.log(memoryCardFront);
+const $cardsWrapper = createCardsWrapper();
+const $memoryCard = createMemoryCard();
+const $memoryCardFront = createMemoryCardFront();
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+
+$root.insertAdjacentElement("beforeend", $cardsWrapper);
+
