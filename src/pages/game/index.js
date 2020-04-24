8c9d714f182983ca52ea54ca9ef4ac677177297d
erdac <tonrc87@gmail.com>
2020-04-23 21:01:34 -0300

    035-3 Removendo o button e a layer

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -5,8 +5,7 @@
     const createMemoryCard = memoryCard.create();
 
     const $pointBar = pointBar.create();
-    const $transparencyLayer = transparencyLayer.render();
-    const $gameButton = gameButton.render();
+    const $layerStart = layerStart.render("Start");
 
     const $memoryCardC = createMemoryCard({
         src: "img/icon-c.png",
@@ -36,7 +35,7 @@
 
     $root.insertAdjacentHTML("afterbegin", $pointBar);
     $root.insertAdjacentElement("beforeend", $cardsWrapper);
-    $root.insertAdjacentHTML("beforeend", $transparencyLayer);
-    $root.insertAdjacentHTML("beforeend", $gameButton);
+    $root.insertAdjacentHTML("beforeend", $layerStart);
+
 
 })();
