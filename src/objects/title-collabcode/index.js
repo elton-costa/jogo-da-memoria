bf8f9d6bb6d88221c3e17aec7fb51943064146ac
erdac <tonrc87@gmail.com>
2020-05-05 18:37:25 -0300

    040-3 HTML do title-collabcode

---
+++ src/objects/title-collabcode/index.js
@@ -0,0 +1,9 @@
+const titleCollabcode = (function () {
+    const module = {};
+
+    module.render = () => `<h1>Welcome!</h1>`;
+
+    return {
+        render: module.render
+    };
+})();
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -4,8 +4,10 @@
     const $loginButton = flatButton.render("Login");
     const $signupButton = flatButton.render("Signup", true);
     const $logoCollabcode = logoCollabcode.render();
+    const $titleCollabcode = titleCollabcode.render();
 
     $root.insertAdjacentHTML("beforeend", $loginButton);
     $root.insertAdjacentHTML("beforeend", $signupButton);
     $root.insertAdjacentHTML("beforeend", $logoCollabcode);
+    $root.insertAdjacentHTML("beforeend", $titleCollabcode);
 })();
