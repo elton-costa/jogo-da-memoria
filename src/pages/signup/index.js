55a65259b9174c939b2e604e1ac3aa77ebd88980
erdac <tonrc87@gmail.com>
2020-05-05 18:13:59 -0300

    040-1 HTML do logo-collabcode

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -3,7 +3,9 @@
 
     const $loginButton = flatButton.render("Login");
     const $signupButton = flatButton.render("Signup", true);
+    const $logoCollabcode = logoCollabcode.render();
 
     $root.insertAdjacentHTML("beforeend", $loginButton);
     $root.insertAdjacentHTML("beforeend", $signupButton);
+    $root.insertAdjacentHTML("beforeend", $logoCollabcode);
 })();
