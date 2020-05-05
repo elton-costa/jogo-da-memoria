86ef509fd4eef5ec0e30d272b23d94682d6bb550
erdac <tonrc87@gmail.com>
2020-05-05 10:42:48 -0300

    039-3 Deixando o flat-button unico

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -1,24 +1,21 @@
 const flatButton = (function () {
     const module = {};
 
+    module._id = 0;
 
-    module._style = () => {
+    module._style = active => {
         const $head = document.querySelector("head");
         const $style = document.createElement("style");
 
         $style.textContent = `
-            .flat-button {
-                background-color: #eae6da;
-                color: #fffcee;
+            .flat-button-${module._id} {
+                background-color: ${active ? "#f25a70" : "#eae6da"};
+                color: ${active ? "#fff" : "#fffcee"};
                 font-size: 24px;
                 font-weight: bold;
                 width: 50%;
                 height: 176px;
                 text-transform: uppercase;
-            }
-            .flat-button.-active {
-                background-color: #f25a70;
-                color: #fff;
             }
         `;
 
@@ -26,10 +23,11 @@ const flatButton = (function () {
     };
 
 
-    module.render = (content = "", variation = "") => {
-        module._style(true);
+    module.render = (content = "", active = false) => {
+        module._id++;
+        module._style(active);
 
-        return `<button class="flat-button ${variation}">${content}</button>`;
+        return `<button class="flat-button-${module._id}">${content}</button>`;
     };
 
     return {
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -2,7 +2,7 @@
     const $root = document.querySelector("#root");
 
     const $loginButton = flatButton.render("Login");
-    const $signupButton = flatButton.render("Signup", "-active");
+    const $signupButton = flatButton.render("Signup", true);
 
     $root.insertAdjacentHTML("beforeend", $loginButton);
     $root.insertAdjacentHTML("beforeend", $signupButton);
