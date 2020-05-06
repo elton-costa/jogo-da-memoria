2696979b3d24f6ad6e05f4ac321c96d36e84578a
erdac <tonrc87@gmail.com>
2020-05-05 21:12:11 -0300

    040-6 Centralizado os children horizontalmente

---
+++ src/objects/logo-wrapper/index.js
@@ -0,0 +1,25 @@
+const logoWrapper = (function () {
+    const module = {};
+
+    module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
+
+        $style.textContent = `
+            .logo-wrapper {
+                text-align: center;
+            }
+        `;
+        $head.insertAdjacentElement("beforeend", $style);
+    }
+
+    module.render = (...$children) => {
+        module._style();
+
+        return `<div class="logo-wrapper">${$children.join("")}</div>`
+    };
+
+    return {
+        render: module.render
+    };
+})();
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -3,12 +3,13 @@
 
     const $loginButton = flatButton.render("Login");
     const $signupButton = flatButton.render("Signup", true);
-    const $avatarWrapper = avatarWrapper.render();
     const $logoCollabcode = logoCollabcode.render();
     const $titleCollabcode = titleCollabcode.render("Welcome!");
 
+    const $logoWrapper = logoWrapper.render($logoCollabcode, $titleCollabcode);
+
     $root.insertAdjacentHTML("beforeend", $loginButton);
     $root.insertAdjacentHTML("beforeend", $signupButton);
-    $root.insertAdjacentHTML("beforeend", $logoCollabcode);
-    $root.insertAdjacentHTML("beforeend", $titleCollabcode);
+    $root.insertAdjacentHTML("beforeend", $logoWrapper);
+    
 })();
