55d497491a4cd3c367037def39f1be55c38b7327
erdac <tonrc87@gmail.com>
2020-05-06 21:03:27 -0300

    041-3 Feito o label-collabcode

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -5,11 +5,12 @@
     const $signupButton = flatButton.render("Signup", true);
     const $logoCollabcode = logoCollabcode.render();
     const $titleCollabcode = titleCollabcode.render("Welcome!");
-
     const $logoWrapper = logoWrapper.render($logoCollabcode, $titleCollabcode);
+    const $labelCollabcode = labelCollabcode.render("E-mail");
 
     $root.insertAdjacentHTML("beforeend", $loginButton);
     $root.insertAdjacentHTML("beforeend", $signupButton);
     $root.insertAdjacentHTML("beforeend", $logoWrapper);
+    $root.insertAdjacentHTML("beforeend", $labelCollabcode);
     
 })();
