84e99cb4719c0e86433402e7a842f29641770e4b
erdac <tonrc87@gmail.com>
2020-05-13 23:18:18 -0300

    044-8 CSS do link-collabcode

--- src/objects/link-collabcode/index.js
+++ src/objects/link-collabcode/index.js
@@ -2,6 +2,24 @@ const linkCollabcode = (function() {
     const module = {};
 
     module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
+
+        $style.textContent = `
+            .link-collabcode {
+                display: block;
+                color: #3a4042;
+                font-size: 14px;
+                text-decoration: none;
+                opacity: 0.7;
+                margin-top: 40px;
+                margin-bottom: 60px;
+                text-align: right;
+            }
+            
+        `;
+
+        $head.insertAdjacentElement("beforeend", $style);
 
     };
 
@@ -9,7 +27,7 @@ const linkCollabcode = (function() {
         module._style();
 
         return `
-            <a href="${href}">${content}</a>
+            <a class="link-collabcode" href="${href}">${content}</a>
         `;
 
     };
