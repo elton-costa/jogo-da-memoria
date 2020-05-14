4780ad3f8332b0bb20daff006985fa3fb2cdbb96
erdac <tonrc87@gmail.com>
2020-05-13 22:08:47 -0300

    044-4 Adicionando botoes de login e signup

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,3 +1,9 @@
 (function () {
     const $root = document.querySelector("#root");
+    
+    const $loginButton = flatButton.render("Login", true);
+    const $signupButton = flatButton.render("Signup", false);
+
+    $root.insertAdjacentHTML("beforeend", $loginButton);
+    $root.insertAdjacentHTML("beforeend", $signupButton);
 })();
